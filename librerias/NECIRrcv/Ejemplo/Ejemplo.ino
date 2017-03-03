f7422040b006f26263ae23314f2af73d525959f0
2Pr04R3p0rt <mathiasbertorelli@gmail.com>
2017-03-03 22:31:02 +0100

    Mas librerias

---
+++ librerias/NECIRrcv/Ejemplo/Ejemplo.ino
@@ -0,0 +1,14 @@
+#include <NECIRrcv.h>
+#define sensor 2; 	// Se define el pin donde conectamos el sensor
+NECIRrcv ir(sensor);
+		
+void setup{
+  Serial.begin(9600); 
+  ir.begin();			
+}
+		
+void loop{
+  unsigned long codigo; // Los datos se almacenan en la variable código 			
+  Serial.print("Código: "); //Se muestra el Código
+  Serial.println(codigo) ;
+}
