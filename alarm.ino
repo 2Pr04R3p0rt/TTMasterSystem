deeeb4dcbdea3a07b7c1a233bb4d1823cac406a7
2Pr04R3p0rt <mathiasbertorelli@gmail.com>
2017-03-04 17:22:19 +0100

    Update alarm.ino

--- alarm.ino
+++ alarm.ino
@@ -3,6 +3,7 @@
 bool pressed; //false = alarma apagada, true = alarma vigilante
 
 Ultrasonic ultrasonic(5,6);
+
 void setup()
 {
   Serial.begin(9600);
@@ -12,15 +13,16 @@ void setup()
   digitalWrite(7, LOW);  // GND mode
   pinMode(8, OUTPUT);
   pinMode(1, INPUT); //aqui llegará la entrada del botón para cambiar los modos
-  pressed = false;
+  pressed = false; //por defecto la alarma está apagada
 }
 
 void loop()
 {
   if (digitalRead(1) == HIGH) //si leemos que ha entrado un boton
   {
-    alarma = !alarma; //cambiamos el modo alarma
+    alarma = true; //cambiamos el modo alarma
   }
+  else alarma = false;
   if (alarma) //tenemos la alarma vigilando la casa, así que cuando el sensor detecte algo sonará la alarma
   {
     if(ultrasonic.Ranging(CM) < 50){
